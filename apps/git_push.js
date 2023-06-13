06a05dcc9798c54da012b8bc653a9945fee0321e
CUZNIL <iamliujunxi@qq.com>
2023-06-13 10:51:41 +0800

    上传测试

---
+++ apps/git_push.js
@@ -0,0 +1,55 @@
+import a from "../model/tools.js"
+
+
+let { name, email, password } = a.getConfig("git")
+
+let intro = `cd plugins/panel-plugin && git config --global user.name "${name}" && git config --global user.email "${email}" && git config --global user.password "${password}" && `
+
+
+export class git_push extends plugin {
+    constructor() {
+        super({
+            name: '上传修改',
+            event: 'message',
+            priority: -233,
+            rule: [
+                {
+                    reg: '^#?上传插件.*$',
+                    fnc: 'git_push',
+                    permission: 'master'
+                },
+            ]
+        })
+    }
+
+
+    async git_push() {
+        let commit = this.e.msg.replace(/#?上传插件/g, "")
+        if (!commit) {
+            this.reply("需要commit信息")
+            return false
+        }
+        let result
+        let cmd
+        cmd = intro + `git add . && git commit -m "${commit}"`
+        console.log(logger.red(cmd))
+        result = await execSync(cmd)
+        logger.mark(`${result.stdout.trim()}\n${logger.red(result.stderr.trim())}`)
+        cmd = intro + `git push`
+        console.log(logger.red(cmd))
+        result = await execSync(cmd)
+        logger.mark(`${result.stdout.trim()}\n${logger.red(result.stderr.trim())}`)
+
+
+        if (ret.error) {
+            logger.error(`远程命令错误：${logger.red(ret.error)}`)
+        }
+    }
+}
+async function execSync(cmd) {
+    return new Promise((resolve, reject) => {
+        exec(cmd, (error, stdout, stderr) => {
+            resolve({ error, stdout, stderr })
+        })
+    })
+}
