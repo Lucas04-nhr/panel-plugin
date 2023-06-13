dc2d430243ac24530b34768dd7e34399bb9bb8bc
CUZNIL <iamliujunxi@qq.com>
2023-06-13 11:09:33 +0800

    上传测试

--- apps/git_push.js
+++ apps/git_push.js
@@ -3,7 +3,7 @@ import a from "../model/tools.js"
 
 let { name, email, password } = a.getConfig("git")
 
-let intro = `cd plugins/panel-plugin && git config --global credential.helper store && git config --global user.name "${name}" && git config --global user.email "${email}" && git config --global user.password "${password}" && `
+let intro = `cd plugins/panel-plugin && git config --global user.name "${name}" && git config --global user.email "${email}" && git config --global user.password "${password}" && `
 
 
 export class git_push extends plugin {
@@ -32,14 +32,20 @@ export class git_push extends plugin {
         let result
         let cmd
         cmd = intro + `git add . && git commit -m "${commit}"`
+        // cmd = "git config -l"
         console.log(logger.red(cmd))
+
         result = await execSync(cmd)
+
         logger.mark(`${result.stdout.trim()}\n${logger.red(result.stderr.trim())}`)
         cmd = intro + `git push`
         console.log(logger.red(cmd))
+
         result = await execSync(cmd)
+
         logger.mark(`${result.stdout.trim()}\n${logger.red(result.stderr.trim())}`)
-        
+
+
     }
 }
 async function execSync(cmd) {
