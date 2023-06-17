664463472c8ccd471dde10c6ea406c62165f3689
cuznil <iamliujunxi@qq.com>
2023-06-17 20:17:28 +0800

    精确强化次数使其合理(不影响任何正常使用，只是看着会正常点。)

--- apps/compate.js
+++ apps/compate.js
@@ -84,70 +84,80 @@ export class compate extends plugin {
                         }
                         docker.mainId = attr_map[docker.main.key][0][0]
 
-                        let list = []
+                        let id_list = []
                         let error = undefined
                         if (docker.star > 2) {
                             //一般都是四挡，除非3星都不到。
+                            let list = []
                             for (let k in docker.attrs) {
                                 let { key, value } = docker.attrs[k]
-                                if (!key) break;
-                                //该词条非空，可以录入该词条非空，可以录入attrIds
+                                if (!key) {
+                                    //该词条为空，可以录入空
+                                    list.push([0, 0])
+                                    continue
+                                }
+                                //该词条非空，可以录入attrIds
                                 value = Number((value / attr_map[key][docker.star]).toFixed(1))
                                 if (value > 1.0 && value < 1.4 || value < 0.7) {
                                     error = `在UID${uid}的面板文件中出现了不可能存在的词条数故跳过该圣遗物，怀疑是星级不合理导致的。\n对应圣遗物是` + logger.red(`星级为${docker.star}的${docker.name}`) + `。请在游戏中检查该用户的${result.avatars[i].name}穿戴的${docker.name}是不是${docker.star}星圣遗物。如果是请提交issue，请务必附带游戏内截图以及本地文件截图。`
                                     break
                                 }
                                 key = Number(docker.star + attr_map[key][0][1]) * 10
+                                list.push({ key, value })
+                            }
+                            let time_list = calcTime(list, docker.star, docker.level)
+                            //计算合理的词条数分布
+                            if (!error)
+                                for (let k in list) {
 
-                                //DELAY:精确强化次数使其合理 
-                                let time = Math.round(value / 0.85)
-                                //理想情况词条数/0.85就是强化次数+1
+                                    let key = list[k].key
+                                    let value = list[k].value
+                                    let time = time_list[k]
 
-                                let left = value * 10 - time * 7
-                                //如果按0.7最低档位分配，还差left/10的词条数未分配
+                                    let left = value * 10 - time * 7
+                                    //如果按0.7最低档位分配，还差left/10的词条数未分配
 
-                                let level = Math.floor(left / time)
-                                //一个一个分配，则至少都是第level+1个档位
+                                    let level = Math.floor(left / time)
+                                    //一个一个分配，则至少都是第level+1个档位
 
-                                let overflow = value * 10 - (level + 7) * time
-                                //如此分配下来，剩几个0.1就有几个词条是需要再+1档位的
+                                    let overflow = value * 10 - (level + 7) * time
+                                    //如此分配下来，剩几个0.1就有几个词条是需要再+1档位的
 
-                                left = time - overflow
-
-                                level++
-                                if (level >= 4) {
-                                    console.log(pluginINFO + "疑似被修改的面板，为避免bug只能调整为正常面板")
-                                    level = 4
-                                    overflow += left
-                                } else {
-                                    while (left--) {
-                                        list.push(key + level)
-                                    }
+                                    left = time - overflow
 
                                     level++
                                     if (level > 4) {
-                                        console.log(pluginINFO + "疑似被修改的面板，为避免bug只能调整为正常面板")
+                                        console.log(pluginINFO + "1.发现被修改的面板，为避免bug只能调整为正常面板")
                                         level = 4
-                                    }
-                                }
-                                while (overflow--) {
-                                    list.push(key + level)
-                                }
+                                        overflow += left
+                                    } else {
+                                        while (left--) {
+                                            id_list.push(key + level)
+                                        }
 
-                            }
+                                        level++
+                                        if (level > 4 && overflow > 0) {
+                                            console.log(pluginINFO + "2.发现被修改的面板，为避免bug只能调整为正常面板")
+                                            level = 4
+                                        }
+                                    }
+                                    while (overflow--) {
+                                        id_list.push(key + level)
+                                    }
+
+                                }
 
                         } else {
                             //TODO:二三档圣遗物([0.8,1.0],[0.7,0.85,1.0])
 
                             error = `暂不支持二星及以下圣遗物的兼容处理。`
-                            break
                         }
 
                         if (error) {
                             console.log(pluginINFO + error)
                             break
                         }
-                        docker.attrIds = list
+                        docker.attrIds = id_list
                         //最后删除旧的主副词条
                         delete docker.main
                         delete docker.attrs
@@ -163,4 +173,51 @@ export class compate extends plugin {
             return `\n${pluginINFO}在处理UID${uid}的数据时：\n${logger.red(e)}`
         }
     }
+
+}
+function calcTime(list, star, level) {
+    let dn = []
+    let up = []
+    let min = 0
+    let max = 0
+    for (let i in list) {
+        let t = Math.ceil(list[i].value)
+        min += t
+        dn.push(t)
+        //最低不会比真实词条数还低
+        t = star > 1 ? 0.7 : 0.8
+        //当星级不到1时，最低档位是0.8。
+        let t2 = Math.floor(list[i].value / t)
+        max += t2
+        up.push(t2)
+        //最高不会比真实词条数/0.7还高(当星级>1时)
+    }
+    let law = Math.floor(level / 4) + star - 1
+    //law:理论最高词条数，对于20级5星是9
+    if (max <= law) {
+        //如果都取最低档位可取
+        return up
+    }
+    if (min >= law - 1) {
+        //如果都取最高档位可取
+        return dn
+    }
+    //都不满足，说明对于20级五星圣遗物：1.max>9不可取 2.min<8不可取。那么就需要调整
+    let sub = law - max
+    //sub:还需要在up的基础上减少几个词条
+    for (let i in up) {
+        let space = up[i] - dn[i]
+        //当前位置可以减少几个词条
+        if (space >= sub) {
+            //如果足够就用了然后退出
+            up[i] -= sub
+            break
+        }
+        else {
+            //如果不够就全用了再找下一个
+            sub -= space
+            up[i] = dn[i]
+        }
+    }
+    return up[i]
 }
